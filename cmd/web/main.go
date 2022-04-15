96f2bf5f3a1b959ecf324fc3a87fc5ac7fcf8610
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2022-04-15 23:51:10 +0700

    update

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,9 +15,10 @@ import (
 	"github.com/golangcollege/sessions"
 )
 
-// Define an application struct to hold the application-wide dependencies for the
-// web application. For now we'll only include fields for the two custom loggers, but
-// we'll add more to it as the build progresses.
+type contextKey string
+
+const contextKeyIsAuthenticated = contextKey("isAuthenticated")
+
 type application struct {
 	errorLog      *log.Logger
 	infoLog       *log.Logger
